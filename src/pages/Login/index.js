c57066b0164e346117fc62bcdf4253550325f8bd
HigoHenrique <99215544+HigoHenrique@users.noreply.github.com>
2023-03-28 14:24:15 -0300

    finalizando projeto de estudo

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -7,10 +7,18 @@ import {
 import {
   Input,
   InputLabel,
-  InputAdornment 
+  InputAdornment
 } from '@material-ui/core';
 
+import { useHistory } from 'react-router-dom';
+import { UsuarioContext } from 'common/context/Usuario';
+import { useContext } from 'react';
+
 function Login() {
+
+  const history = useHistory();
+  const { nome, setNome, saldo, setSaldo } = useContext(UsuarioContext);
+
   return (
     <Container>
       <Titulo>
@@ -21,6 +29,8 @@ function Login() {
           Nome
         </InputLabel>
         <Input
+          value={nome}
+          onChange={(event) => setNome(event.target.value)}
           type="text"
         />
       </InputContainer>
@@ -29,20 +39,25 @@ function Login() {
           Saldo
         </InputLabel>
         <Input
-        type="number"
-        startAdornment={
-          <InputAdornment position="start">
-            R$
-          </InputAdornment>
-        }
-      />
+          type="number"
+          value={saldo}
+          onChange={(event) => setSaldo(event.target.value)}
+          startAdornment={
+            <InputAdornment position="start">
+              R$
+            </InputAdornment>
+          }
+        />
       </InputContainer>
       <Button
         variant="contained"
         color="primary"
+        disabled={nome.length < 3}
+        onClick={() => { history.push('/feira') }}
       >
         Avançar
       </Button>
+
     </Container>
   )
 };
